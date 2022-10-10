73e5b083fc9caf5b1bf5d8161be182fd500e1dd1
Urias01 <uriasluis64@gmail.com>
2022-10-10 18:28:04 -0300

    html e css login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
   {
     path: '', component: NavComponent,children: [
       { path: 'home', component: HomeComponent },
