ca45767e1f7fa4601eb5943d6b553c0a721550a1
Urias01 <uriasluis64@gmail.com>
2022-10-10 17:05:22 -0300

    cria componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,9 +30,10 @@ import { MatCardModule } from '@angular/material/card';
 
 /* Componentes do Projeto */
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
