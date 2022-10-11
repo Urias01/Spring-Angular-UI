6908fb2dae9e720799c28cae5cbf0e82334d8377
Urias01 <uriasluis64@gmail.com>
2022-10-11 19:05:49 -0300

    Sistema Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   {
-    path: '', component: NavComponent,children: [
+    path: '', component: NavComponent, canActivate:[AuthGuard], children: [
       { path: 'home', component: HomeComponent },
       { path: 'tecnicos', component: TecnicoListComponent}
     ],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,38 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { Credenciais } from 'src/app/models/credenciais'
+import { Credenciais } from 'src/app/models/credenciais';
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
   creds: Credenciais = {
     email: '',
-    password: ''
-  }
+    password: '',
+  };
 
   email = new FormControl(null, Validators.email);
   password = new FormControl(null, Validators.minLength(3));
 
-  constructor(private toast: ToastrService) { }
+  constructor(private toast: ToastrService, private service: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   logar() {
-    this.toast.error('Usuário e/ou Senha Inválidos', 'Login');
-    this.creds.password = "";
+    this.service.authenticate(this.creds).subscribe(response => {
+      this.service.successFulLogin(response.headers.get('Authorization').substring(7));
+      this.router.navigate(['']);
+    }, () => {
+      this.toast.error("Usuário e/ou Senha Inválidos");
+    });
   }
 
   validaCampos(): boolean {
-    if(this.email.valid && this.password.valid) {
-      return true;
-    } else {
-      return false;
-    }
+    return this.email.valid && this.password.valid;
   }
-
 }
