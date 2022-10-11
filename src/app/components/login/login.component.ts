4c98d84776f2a06d99329ab25fb790601c72d79f
Urias01 <uriasluis64@gmail.com>
2022-10-11 13:55:40 -0300

    valida form login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, Validators } from '@angular/forms';
+import { Credenciais } from 'src/app/models/credenciais'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  creds: Credenciais = {
+    email: '',
+    password: ''
+  }
+
+  email = new FormControl(null, Validators.email);
+  password = new FormControl(null, Validators.minLength(3));
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validaCampos(): boolean {
+    if(this.email.valid && this.password.valid) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
