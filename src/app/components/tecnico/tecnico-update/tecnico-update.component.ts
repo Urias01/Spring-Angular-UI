ffd0fed33b312b63188deed8aca64da446df892f
Urias01 <uriasluis64@gmail.com>
2022-10-20 17:50:58 -0300

    finaliza component tecnico update

--- src/app/components/tecnico/tecnico-update/tecnico-update.component.ts
+++ src/app/components/tecnico/tecnico-update/tecnico-update.component.ts
@@ -1,15 +1,80 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 @Component({
   selector: 'app-tecnico-update',
   templateUrl: './tecnico-update.component.html',
-  styleUrls: ['./tecnico-update.component.css']
+  styleUrls: ['./tecnico-update.component.css'],
 })
 export class TecnicoUpdateComponent implements OnInit {
+  tecnico: Tecnico = {
+    id: '',
+    name: '',
+    cpf: '',
+    email: '',
+    password: '',
+    perfis: [],
+    dataCriacao: '',
+  };
+  name: FormControl = new FormControl(null, Validators.minLength(3));
+  cpf: FormControl = new FormControl(null, Validators.required);
+  email: FormControl = new FormControl(null, Validators.email);
+  password: FormControl = new FormControl(null, Validators.minLength(3));
 
-  constructor() { }
+  constructor(
+    private service: TecnicoService,
+    private toast: ToastrService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.tecnico.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
   }
 
+  findById(): void {
+    this.service.findById(this.tecnico.id).subscribe(response => {
+      response.perfis = [];
+      this.tecnico = response;
+    });
+  }
+
+  update() {
+    this.service.update(this.tecnico).subscribe(
+      () => {
+        this.toast.success('Técnico atualizado com Sucesso', 'Update');
+        this.router.navigate(['tecnicos']);
+      },
+      (ex) => {
+        if (ex.error.erros) {
+          ex.error.erros.forEach((err) => {
+            this.toast.error(err.message);
+          });
+        } else {
+          this.toast.error(ex.error.message);
+        }
+      }
+    );
+  }
+
+  addPerfil(perfil: any): void {
+    if (this.tecnico.perfis.includes(perfil)) {
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1);
+    } else {
+      this.tecnico.perfis.push(perfil);
+    }
+  }
+
+  validaCampos(): boolean {
+    return (
+      this.name.valid &&
+      this.cpf.valid &&
+      this.email.valid &&
+      this.password.valid
+    );
+  }
 }
