23698479a433f1719655d640b2543040a49b2260
Urias01 <uriasluis64@gmail.com>
2022-10-20 17:23:49 -0300

    finaliza tecnico create

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,9 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -7,14 +11,53 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./tecnico-create.component.css'],
 })
 export class TecnicoCreateComponent implements OnInit {
+  tecnico: Tecnico = {
+    id: '',
+    name: '',
+    cpf: '',
+    email: '',
+    password: '',
+    perfis: [],
+    dataCriacao: '',
+  };
   name: FormControl = new FormControl(null, Validators.minLength(3));
   cpf: FormControl = new FormControl(null, Validators.required);
   email: FormControl = new FormControl(null, Validators.email);
   password: FormControl = new FormControl(null, Validators.minLength(3));
 
-  constructor() {}
+  constructor(
+    private service: TecnicoService,
+    private toast: ToastrService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
+
+  create() {
+    this.service.create(this.tecnico).subscribe(
+      () => {
+        this.toast.success('Técnico cadastrado com Sucesso', 'Cadastro');
+        this.router.navigate(['tecnicos']);
+      },
+      (ex) => {
+        if (ex.error.erros) {
+          ex.error.erros.forEach((err) => {
+            this.toast.error(err.message);
+          });
+        } else {
+          this.toast.error(ex.error.message);
+        }
+      }
+    );
+  }
+
+  addPerfil(perfil: any): void {
+    if (this.tecnico.perfis.includes(perfil)) {
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1);
+    } else {
+      this.tecnico.perfis.push(perfil);
+    }
+  }
 
   validaCampos(): boolean {
     return (
