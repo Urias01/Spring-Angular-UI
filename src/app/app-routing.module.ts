874fdd7c6e6007d483480d18285af041dbb33f2b
Urias01 <uriasluis64@gmail.com>
2022-10-26 18:04:55 -0300

    Rota para chamado-update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,24 +14,26 @@ import { ClienteCreateComponent } from './components/client/client-create/client
 import { ClienteUpdateComponent } from './components/client/client-update/client-update.component';
 import { ClienteListComponent } from './components/client/client-list/client-list.component';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
+import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   {
     path: '', component: NavComponent, canActivate:[AuthGuard], children: [
-      { path: 'home',                           component: HomeComponent },
-      { path: 'tecnicos',                 component: TecnicoListComponent},
-      { path: 'tecnicos/create',        component: TecnicoCreateComponent},
-      { path: 'tecnicos/update/:id',    component: TecnicoUpdateComponent},
-      { path: 'tecnicos/delete/:id',    component: TecnicoDeleteComponent},
+      { path: 'home',                               component: HomeComponent },
+      { path: 'tecnicos',                     component: TecnicoListComponent},
+      { path: 'tecnicos/create',            component: TecnicoCreateComponent},
+      { path: 'tecnicos/update/:id',        component: TecnicoUpdateComponent},
+      { path: 'tecnicos/delete/:id',        component: TecnicoDeleteComponent},
 
-      { path: 'clientes',                 component: ClienteListComponent},
-      { path: 'clientes/create',        component: ClienteCreateComponent},
-      { path: 'clientes/update/:id',    component: ClienteUpdateComponent},
-      { path: 'clientes/delete/:id',    component: ClienteDeleteComponent},
+      { path: 'clientes',                     component: ClienteListComponent},
+      { path: 'clientes/create',            component: ClienteCreateComponent},
+      { path: 'clientes/update/:id',        component: ClienteUpdateComponent},
+      { path: 'clientes/delete/:id',        component: ClienteDeleteComponent},
 
-      { path: 'chamados',                 component: ChamadoListComponent},
-      { path: 'chamados/create',        component: ChamadoCreateComponent},
+      { path: 'chamados',                     component: ChamadoListComponent},
+      { path: 'chamados/create',            component: ChamadoCreateComponent},
+      { path: 'chamados/update/:id',        component: ChamadoUpdateComponent},
     ],
   },
 ];
